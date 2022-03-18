1a0703b836eb6efaec07a5ffbfccdc5edd4a3e75
Arun <arun49804@gmail.com>
2022-03-18 14:09:45 +0530

    Write backend for company search

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ async function testStudentProfileData(){
     const refresh = await StudentProfileModel.deleteMany({}) //refresh the table on each restart
 
     const studentProfile = await StudentProfileModel.create({
-        enrollmentNumber: "BTBT",
+        enrollmentNumber: "BT18CSE031",
         
         studentInfo:{
             name: "AAA",
@@ -86,6 +86,44 @@ async function companyProfileData(){
     const companyProfile = await companyprofiles.create({
         name: "micro",
         id: "10222",
+        logo: "BBB",
+        companySocial:{
+            linkedin: {
+                name: "LINKEDIN",
+                link: "LINKEDIN LINK"
+            },
+            website: {
+                name: "WEBSITE"
+            }
+        },
+        placementData: [
+            {year:1111, students:2222, ctc:1},
+            {year:2222, students:3333, ctc:2}
+        ]
+    })
+
+    const companyProfile2 = await companyprofiles.create({
+        name: "micro",
+        id: "10223",
+        logo: "BBB",
+        companySocial:{
+            linkedin: {
+                name: "LINKEDIN",
+                link: "LINKEDIN LINK"
+            },
+            website: {
+                name: "WEBSITE"
+            }
+        },
+        placementData: [
+            {year:1111, students:2222, ctc:1},
+            {year:2222, students:3333, ctc:2}
+        ]
+    })
+
+    const companyProfile3 = await companyprofiles.create({
+        name: "microsoft",
+        id: "10224",
         logo: "BBB",
         companySocial:{
             linkedin: {
