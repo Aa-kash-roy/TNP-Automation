ec9692d5b0fc3a682da5f9310cea1776324982be
Akash <meakash0102@gmail.com>
2022-03-29 17:12:31 +0530

    changing app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import React from "react"
 import express from "express"
-import {renderToString} from "react-dom/server.js"
+// import {renderToString} from "react-dom/server.js"
 
-import TestFun from "./views/portal.jsx"
+// import TestFun from "./views/portal.jsx"
 
 
-import { testStudentProfileData } from "./dummydata/students.js"
-import { companyProfileData } from "./dummydata/companies.js"
-import { coreStudentProfileData } from "./dummydata/corestudents.js"
-import { newPlacementData } from "./dummydata/newplacements.js"
+// import { testStudentProfileData } from "./dummydata/students.js"
+// import { companyProfileData } from "./dummydata/companies.js"
+// import { coreStudentProfileData } from "./dummydata/corestudents.js"
+// import { newPlacementData } from "./dummydata/newplacements.js"
 
 
 import bodyParser from "body-parser"
@@ -16,22 +16,22 @@ import mongoose from "mongoose"
 import {PORT, MONGO_URL} from "./config"
 
 
-//user views
-const users = require('./auth/controllers/UserController.js')
-const student = require('./StudentProfile/controllers/StudentProfileController.js')
-const company = require('./CompanyProfile/controllers/CompanyProfileController.js')
-const practice = require('./CodingPractice/controller/codingHandler.js')
-const test = require('./views/test.js')
-const placement = require('./PlacementPage/controller.js')
+// user views
+// const users = require('./auth/controllers/UserController.js')
+// const student = require('./StudentProfile/controllers/StudentProfileController.js')
+// const company = require('./CompanyProfile/controllers/CompanyProfileController.js')
+// const practice = require('./CodingPractice/controller/codingHandler.js')
+// const test = require('./views/test.js')
+// const placement = require('./PlacementPage/controller.js')
 
 
 //admin views
-const adminlanding = require("./Admin/Landing/controller.js")
-const yeardisplay = require("./Admin/Years/controller.js")
-const adminannounce = require("./Admin/Announce/controller.js")
-const newplacement = require("./Admin/NewPlacement/controller.js")
-const admincompanysearch = require("./Admin/CompanySearch/controller.js")
-const placementreports = require("./Admin/PlacementReports/controller.js")
+// const adminlanding = require("./Admin/Landing/controller.js")
+// const yeardisplay = require("./Admin/Years/controller.js")
+// const adminannounce = require("./Admin/Announce/controller.js")
+// const newplacement = require("./Admin/NewPlacement/controller.js")
+// const admincompanysearch = require("./Admin/CompanySearch/controller.js")
+// const placementreports = require("./Admin/PlacementReports/controller.js")
 
 
 
@@ -46,22 +46,22 @@ app.use(express.static(__dirname + "/public"));
 
 
 //user views
-app.use('/users', users) 
-app.use('/student', student)
-app.use('/company', company)
-app.use('/practice', practice)
-app.use('/', test)
-app.use('/placements', placement)
+// app.use('/users', users) 
+// app.use('/student', student)
+// app.use('/company', company)
+// app.use('/practice', practice)
+// app.use('/', test)
+// app.use('/placements', placement)
 
 
 //admin views
-app.use('/admin', adminlanding)
-app.use('/admin/year', yeardisplay)
-app.use('/admin/announce', adminannounce)
-app.use('/admin/newplacement', newplacement)
-app.use('/', yeardisplay)
-app.use('/admin/companysearch', admincompanysearch)
-app.use('/admin/placementreports', placementreports)
+// app.use('/admin', adminlanding)
+// app.use('/admin/year', yeardisplay)
+// app.use('/admin/announce', adminannounce)
+// app.use('/admin/newplacement', newplacement)
+// app.use('/', yeardisplay)
+// app.use('/admin/companysearch', admincompanysearch)
+// app.use('/admin/placementreports', placementreports)
 
 // connect to the database
 
@@ -70,29 +70,29 @@ const mongoDbConnect = () =>{
 }
 
 
-//initialize dummy data
-testStudentProfileData()
-companyProfileData()
-coreStudentProfileData()
-newPlacementData()
+// //initialize dummy data
+// testStudentProfileData()
+// companyProfileData()
+// coreStudentProfileData()
+// newPlacementData()
 
 
 
 app.get('/', (req, res) =>{
     res.send("Hoiiiiiiiiiiii")
 })
-app.get('/portal', function(req, res){
-    let reactComp = renderToString(<TestFun/>);
-    res.render('portal', {reactApp: reactComp});
-})
+// app.get('/portal', function(req, res){
+//     let reactComp = renderToString(<TestFun/>);
+//     res.render('portal', {reactApp: reactComp});
+// })
 
-app.post('/portal', function(req, res){
-    let user = req.body.name_input
-    let pass = req.body.password_input
-    console.log(user)
-    console.log(pass)
-    res.redirect('/')
-})
+// app.post('/portal', function(req, res){
+//     let user = req.body.name_input
+//     let pass = req.body.password_input
+//     console.log(user)
+//     console.log(pass)
+//     res.redirect('/')
+// })
 
 
 const start = async() =>{
