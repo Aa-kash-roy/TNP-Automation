f5b9b3ddb6facbbf38a24da0973591c4c95c194d
Arun <arun49804@gmail.com>
2022-03-30 14:33:52 +0530

    Add validations for application

--- PlacementPage/controller.js
+++ PlacementPage/controller.js
@@ -27,10 +27,6 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
     try{
         const enrollmentNumber = "BT18CSE031" //change this to get actual roll number
-        const upd = await StudentProfileModel.findOneAndUpdate({enrollmentNumber: enrollmentNumber}, { $push: {
-            applications: req.body['applicationId']
-        }}, {new: true}
-        )
 
         const coreDetails = await corestudentprofiles.findOne({enrollmentNumber: enrollmentNumber}).lean()
 
@@ -42,41 +38,73 @@ router.post('/', async (req, res, next) => {
 
         const resumeSourcePath = "./StudentProfile/resumes/" + enrollmentNumber + ".pdf" 
         const resumeDestPath = './Applications/' + jobDetails.name + " " + jobDetails.role + " " + jobDetails._id.toString() + "/resumes/" + enrollmentNumber + ".pdf"
- 
+        const studentYear = parseInt((coreDetails.studentInfo.semester+1)/2).toString()
+
         let cond1 = coreDetails.studentInfo.cgpa >= jobDetails.minCGPA
         let cond2 = coreDetails.backlogs <= jobDetails.maxBacklogs
         let cond3 = jobDetails.gender.includes(coreDetails.studentInfo.gender)
+        let cond4 = jobDetails.eligibleYears.includes(studentYear)
 
-        
+        let cond5;
+        if(jobDetails.role == "Full-time"){
+            if('compensation' in coreDetails.placement && coreDetails.placement.compensation*1.5 <= jobDetails.ctc)
+                cond5 = true
+            else
+                cond5 = false
+        }
+        else{
+            if(coreDetails.internship.internshipCompleted == "Pending")
+                cond5 = true
+            else
+                cond5 = false
+        }
 
-        
+        if(jobDetails.allowAll) cond5 = true
 
-        const path = './Applications/' + jobDetails.name + " " + jobDetails.role + " " + jobDetails._id.toString() + "/details.xlsx"
-        const file = xlsx.readFile(path)
-        let sheet = file.Sheets[file.SheetNames[0]]
-        xlsx.utils.sheet_add_aoa(sheet, [[
-            coreDetails.enrollmentNumber,
-            coreDetails.studentInfo.name,
-            coreDetails.studentInfo.cgpa,
-            coreDetails.backlogs,
-            coreDetails.studentInfo.branch,
-            coreDetails.studentInfo.semester,
-            coreDetails.studentInfo.gender,
-            coreDetails.internship.internshipCompleted,
-            coreDetails.placement.isPlaced
-        ]], {origin: "A"+(jobDetails.numberOfApplications+1)})
-        file.Sheets[file.SheetNames[0]] = sheet;
-        xlsx.writeFile(file, path);
+        let cond6 = fs.existsSync(resumeSourcePath)
 
 
-        fs.copyFile(resumeSourcePath, resumeDestPath, (err) => {
-            if (err) throw err;
-            console.log('source.txt was copied to destination.txt');
-        });
+        console.log("CGPA" + cond1)
+        console.log("BACKLOG" + cond2)
+        console.log("GENDER" + cond3)
+        console.log("YEAR" + cond4)
+        console.log("ALLOW" + cond5)
+        console.log("RESUME" + cond6)
 
+        if(cond1 && cond2 && cond3 && cond4 && cond5 && cond6){     
+            
+            const upd = await StudentProfileModel.findOneAndUpdate({enrollmentNumber: enrollmentNumber}, { $push: {
+                applications: req.body['applicationId']
+            }}, {new: true}
+            )
 
+            const path = './Applications/' + jobDetails.name + " " + jobDetails.role + " " + jobDetails._id.toString() + "/details.xlsx"
+            const file = xlsx.readFile(path)
+            let sheet = file.Sheets[file.SheetNames[0]]
+            xlsx.utils.sheet_add_aoa(sheet, [[
+                coreDetails.enrollmentNumber,
+                coreDetails.studentInfo.name,
+                coreDetails.studentInfo.cgpa,
+                coreDetails.backlogs,
+                coreDetails.studentInfo.branch,
+                coreDetails.studentInfo.semester,
+                coreDetails.studentInfo.gender,
+                coreDetails.internship.internshipCompleted,
+                coreDetails.placement.isPlaced
+            ]], {origin: "A"+(jobDetails.numberOfApplications+1)})
+            file.Sheets[file.SheetNames[0]] = sheet;
+            xlsx.writeFile(file, path);
 
-        res.redirect("/placements")
+            fs.copyFile(resumeSourcePath, resumeDestPath, (err) => {
+                if (err) throw err;
+                console.log('source.txt was copied to destination.txt');
+            });
+
+            res.redirect("/placements")
+        }
+        else{
+            res.redirect("/placements")
+        }
     }
     catch(e){
         console.log(e)
